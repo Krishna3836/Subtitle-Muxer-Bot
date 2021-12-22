a7e0fdbb609fc11ef7d50f911e776f85f32625eb
Naysa Bots <96517910+Naysabots@users.noreply.github.com>
2021-12-22 13:09:00 +0530

    Create translation.py

---
+++ translation.py
@@ -0,0 +1,10 @@
+class Translation (object):   
+    
+    DOWNLOAD_SUCCESS = """File downloaded successfully!
+
+Time taken : {} seconds."""
+    FILE_SIZE_ERROR = "ERROR : Cannot Extract File Size from URL!"
+    MAX_FILE_SIZE = "File size is greater than 2Gb. Which is the limit imposed by telegram!"
+    LONG_CUS_FILENAME = """Filename you provided is greater than 60 characters.
+Please provide a shorter name."""
+    UNSUPPORTED_FORMAT = "ERROR : File format {} Not supported!"
