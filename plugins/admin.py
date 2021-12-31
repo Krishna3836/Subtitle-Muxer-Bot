960deae3c5c72a23d96ff32b572446157a49e42b
Naysa Bots <96517910+Naysabots@users.noreply.github.com>
2021-12-31 14:53:43 +0530

    Update admin.py

--- plugins/admin.py
+++ plugins/admin.py
@@ -8,12 +8,12 @@ from pyrogram import Client as Tellybots
 from pyrogram import filters
 
 
-from database.access import tellybots
+from database.database import db
 
 @Tellybots.on_message(filters.private & filters.command('total'))
 async def sts(c, m):
     if m.from_user.id != Config.OWNER_ID:
         return 
-    total_users = await tellybots.total_users_count()
+    total_users = await db.total_users_count()
     await m.reply_text(text=f"Total user(s) {total_users}", quote=True)
 
