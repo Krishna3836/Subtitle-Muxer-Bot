8515863914e39328c19c40a28bfb6624079e3fe2
Naysa Bots <96517910+Naysabots@users.noreply.github.com>
2021-12-31 14:14:21 +0530

    Update adduser.py

--- database/adduser.py
+++ database/adduser.py
@@ -1,9 +1,15 @@
 from pyrogram import Client
 from database.access import tellybots
 from pyrogram.types import Message
+from config import Config
 
+async def AddUser(bot: Client, cmd: Message):
+    if not await tellybots.is_user_exist(cmd.from_user.id):
+        await tellybots.add_user(cmd.from_user.id)
+        if Config.LOG_CHANNEL is not None:
+            await bot.send_message(
+                int(Config.LOG_CHANNEL),
+                f"#NEW_USER: \n\nNew User [{cmd.from_user.first_name}](tg://user?id={cmd.from_user.id}) started @{(await bot.get_me()).username} !!"
+            )
 
-async def AddUser(bot: Client, update: Message):
-    if not await tellybots.is_user_exist(update.from_user.id):
-           await tellybots.add_user(update.from_user.id)
 
