7bb4533e1d0520307650e3fddcfc33e0f37698ae
Naysa Bots <96517910+Naysabots@users.noreply.github.com>
2021-12-31 18:21:29 +0530

    Update translation.py

--- plugins/translation.py
+++ plugins/translation.py
@@ -1,6 +1,6 @@
-class Translation (object):   
-    
 
+from pyrogram.emoji import *   
+class Translation (object):   
     DOWNLOAD_SUCCESS = """File downloaded successfully!
 
 Time taken : {} seconds."""
@@ -10,3 +10,12 @@ Time taken : {} seconds."""
 Please provide a shorter name."""
     UNSUPPORTED_FORMAT = "ERROR ♨️♨️♨️ : File format {} Not supported!"
     CHOOSE_CMD = "Subtitle file downloaded successfully!.\nChoose your desired mixing!\n[ /softremove , /softmux , /hardmux ]"
+    DOWNLOAD_START = "Trying To Download 📩"
+    UPLOAD_START = "Uploading Please Wait 🌀"
+    UPLOAD_SUCESS = "Thanks for using Me @Tellybots 🥳"
+    BANNED_USER_TEXT = "Hey bro, you are **banned** from using me.😡"
+    NOT_LOGGED_TEXT = "This bot was only for private use. If you want to use this bot you need to send me correct password in the format `/login bot_password`"
+    SAVED_CUSTOM_THUMBNAIL = "Thumbnail Saved Permanently ✅"
+    DELETED_CUSTOM_THUMBNAIL = "Thumbnail Deleted Successfully ✅"
+    NO_CUSTOM_THUMB_NAIL_FOUND = "𝖭𝗈 T𝗁𝗎𝗆𝖻𝗇𝖺𝗂𝗅 𝖥𝗈𝗎𝗇𝖽 🖼️"
+    THUMBNAIL_CAPTION = f"{BACKHAND_INDEX_POINTING_UP_LIGHT_SKIN_TONE} Your Permanent thumbnail"
