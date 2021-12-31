4305ed73808579c3292e06199e11c32054c7102b
Naysa Bots <96517910+Naysabots@users.noreply.github.com>
2021-12-31 12:41:50 +0530

    Create script.py

---
+++ plugins/script.py
@@ -0,0 +1,68 @@
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+
+class Script(object):
+
+
+
+    START_TEXT = """
+Hey {} 
+
+I am Telegram Most Powerful Subtitle Muxer Bot
+
+I can Mux Any Srt or Txt File in File or Video
+
+Use Help Command to Know How to Use me
+
+Made With 💕 By @Tellybots_4u
+"""
+    HELP_TEXT = """
+Recommended
+➠ Use Hardmux If You Have More Time
+
+Recommended
+➠ Use Softmux To add Subtitle Fastly in It
+
+Softmux
+➠ Send /softmux to add Subtitle Softly in it
+
+HardMux
+➠ Send /hardmux to add Subtitle hardly in it 
+
+Softremove
+➠ Send /softremove to extract subtitles from softmuxed video using that sub file
+
+Made With 💕 By @Tellybots_4u
+"""
+    ABOUT_TEXT = """
+ **🤖 Bot :** Sub-Muxer\n
+ **👲 Developer :** [Tellybots_4u](https://telegram.me/tellybots_4u)\n
+ **👥 Channel :** [Tellybots_4u](https://telegram.me/tellybots_4u)\n
+ **❄️ Credits :** Everyone in this journey\n
+ **🍴 Source :** [Click here](https://t.me/tellybots_digital)\n
+ **📝 Language :** [Python3](https://python.org)\n
+ **📚 Library :** [Pyrogram v1.2.0](https://pyrogram.org)\n
+ **🌟 Server :** [Heroku](https://heroku.com)\n
+"""
+    START_BUTTONS = InlineKeyboardMarkup(
+        [[
+        InlineKeyboardButton('🤖 Update Channel', url='https://telegram.me/tellybots_4u'),
+        InlineKeyboardButton('💬 Support Group', url='https://telegram.me/tellybots_support')
+        ],[
+        InlineKeyboardButton('❔ Help', callback_data='help'),
+        InlineKeyboardButton('⛔ Close', callback_data='close')
+        ]]
+    )
+    HELP_BUTTONS = InlineKeyboardMarkup(
+        [[
+        InlineKeyboardButton('🏡 Home', callback_data='home'),
+        InlineKeyboardButton('👲 About', callback_data='about'),
+        InlineKeyboardButton('⛔ Close', callback_data='close')
+        ]]
+    )
+    ABOUT_BUTTONS = InlineKeyboardMarkup(
+        [[
+        InlineKeyboardButton('🏡 Home', callback_data='home'),
+        InlineKeyboardButton('❔ Help', callback_data='help'),
+        InlineKeyboardButton('⛔ Close', callback_data='close')
+        ]]
+    )
